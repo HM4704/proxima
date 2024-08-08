0037257f03f205378e6f6b6b10975b6add82785c
HM4704 <hermann.moosburger@gmail.com>
2024-08-08 10:46:43 +0200

    first structs

--- api/api.go
+++ api/api.go
@@ -20,6 +20,62 @@ const (
 	PathGetLatestReliableBranch = "/get_latest_reliable_branch"
 )
 
+type Error struct {
+	// empty string when no error
+	Error string `json:"error,omitempty"`
+}
+
+type LedgerID struct {
+	Error
+	// hex-encoded ledger id bytes
+	LedgerIDBytes string `json:"ledger_id_bytes,omitempty"`
+}
+
+// OutputList is returned by 'get_account_outputs'
+type OutputList struct {
+	Error
+	// key is hex-encoded outputID bytes
+	// value is hex-encoded raw output data
+	Outputs map[string]string `json:"outputs,omitempty"`
+}
+
+// ChainOutput is returned by 'get_chain_output'
+type ChainOutput struct {
+	Error
+	// hex-encoded outputID
+	OutputID string `json:"output_id,omitempty"`
+	// hex-encoded output data
+	OutputData string `json:"output_data,omitempty"`
+}
+
+// OutputData is returned by 'get_output'
+type OutputData struct {
+	Error
+	// hex-encoded output data
+	OutputData string `json:"output_data,omitempty"`
+	//Inclusion  []InclusionDataEncoded `json:"inclusion,omitempty"`
+}
+
+type QueryTxStatus struct {
+	Error
+	TxIDStatus vertex.TxIDStatusJSONAble       `json:"txid_status"`
+	Inclusion  *multistate.TxInclusionJSONAble `json:"inclusion,omitempty"`
+}
+
+type TxInclusionScore struct {
+	ThresholdNumerator   int `json:"threshold_numerator"`
+	ThresholdDenominator int `json:"threshold_denominator"`
+	LatestSlot           int `json:"latest_slot"`
+	EarliestSlot         int `json:"earliest_slot"`
+	StrongScore          int `json:"strong_score"`
+	WeakScore            int `json:"weak_score"`
+}
+
+type QueryTxInclusionScore struct {
+	Error
+	TxInclusionScore
+}
+
 type (
 	Error struct {
 		// empty string when no error
@@ -109,6 +165,25 @@ type (
 		RootData multistate.RootRecordJSONAble `json:"root_record,omitempty"`
 		BranchID ledger.TransactionID          `json:"branch_id,omitempty"`
 	}
+
+	SequencerData struct {
+		SequencerID    ledger.ChainID
+		LedgerCoverage uint64
+		SlotInflation  uint64
+		Supply         uint64
+		AmountOnChain  uint64
+	}
+	
+	SequencerStatistic {
+		active bool
+		wins   int
+		sumInflation int64
+	}
+	
+	SequencerStatistics struct {
+		sequStat map[string]SequencerStatistic
+	}
+	
 )
 
 const ErrGetOutputNotFound = "output not found"
@@ -142,4 +217,9 @@ func CalcTxInclusionScore(inclusion *multistate.TxInclusion, thresholdNumerator,
 		ret.StrongScore = (numIncludedInDominating * 100) / numDominatingBranches
 	}
 	return ret
+}
+
+
+func GetSequencerStatistics(int nSlotsBack) SequencerStatistics {
+
 }
